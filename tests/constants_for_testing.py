01b259dd7cf3b26c6a6be4fc9906b09daba50854
tomer gelber <tomergelber@gmail.com>
2014-05-10 14:23:33 +0300

    now we loading username and password for testing from json.

--- tests/constants_for_testing.py
+++ tests/constants_for_testing.py
@@ -1,4 +1,9 @@
+import json
+from os import path
+
+
+TEST_SETTINGS = path.join(path.dirname(__file__), "account_settings.json")
+ACCOUNT_TEST_USERNAME, ACCOUNT_TEST_PASSWORD = json.load(open(TEST_SETTINGS, "r")).values()
+
 ANIME_ID = 1887
 MANGA_ID = 587
-ACCOUNT_TEST_USERNAME = "tomeriko"
-ACCOUNT_TEST_PASSWORD = "8348711aB"
