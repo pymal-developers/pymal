675fa901613d87e4eee51bc88db611b8f2892ec8
tomer gelber <tomergelber@gmail.com>
2014-05-15 19:06:48 +0300

    added type tests for my anime and change lists names.

--- tests/test_my_anime.py
+++ tests/test_my_anime.py
@@ -9,8 +9,45 @@ class MyAnimeTestCase(unittest.TestCase):
         cls.account = Account(ACCOUNT_TEST_USERNAME, ACCOUNT_TEST_PASSWORD)
         cls.anime = cls.account.animes[0]
 
-    def test_anime_title(self):
-        self.anime.my_score
+    def test_my_anime_my_id(self):
+        self.assertIsInstance(self.anime.__my_id, int)
+
+    def test_my_anime_my_status(self):
+        self.assertIsInstance(self.anime.__my_status, int)
+
+    def test_my_anime_my_is_rewatching(self):
+        self.assertIsInstance(self.anime.__my_is_rewatching, bool)
+
+    def my_completed_episodes(self):
+        self.assertIsInstance(self.anime.__my_completed_episodes, int)
+
+    def test_my_anime_my_score(self):
+        self.assertIsInstance(self.anime.__my_score, int)
+
+    def test_my_anime_my_start_date(self):
+        self.assertTrue(self.anime.__my_start_date == float('inf') or type(self.anime.__my_start_date) == int)
+
+    def test_my_anime_my_end_date(self):
+        self.assertTrue(self.anime.__my_end_date == float('inf') or type(self.anime.__my_end_date) == int)
+
+    def test_my_anime_my_priority(self):
+        self.assertIsInstance(self.anime.__my_priority, int)
+
+    def test_my_anime_my_storage_type(self):
+        self.assertIsInstance(self.anime.__my_storage_type, int)
+
+    def test_my_anime_my_storage_value(self):
+        self.assertIsInstance(self.anime.__my_storage_value, int)
+
+    def test_my_anime_my_download_episodes(self):
+        self.assertIsInstance(self.anime.__my_download_episodes, int)
+
+    def test_my_anime_my_times_rewatched(self):
+        self.assertIsInstance(self.anime.__my_times_rewatched, int)
+
+    def test_my_anime_my_rewatch_value(self):
+        self.assertIsInstance(self.anime.__my_rewatch_value, int)
+
 
 def main():
     unittest.main()
