e74816c16850c246d56799332146dc2bbd2551f8
tomer gelber <tomergelber@gmail.com>
2014-05-15 19:17:56 +0300

    added type tests for my manga and change lists names.

--- tests/test_my_anime.py
+++ tests/test_my_anime.py
@@ -10,43 +10,43 @@ class MyAnimeTestCase(unittest.TestCase):
         cls.anime = cls.account.animes[0]
 
     def test_my_anime_my_id(self):
-        self.assertIsInstance(self.anime.__my_id, int)
+        self.assertIsInstance(self.anime.my_id, int)
 
     def test_my_anime_my_status(self):
-        self.assertIsInstance(self.anime.__my_status, int)
+        self.assertIsInstance(self.anime.my_status, int)
 
     def test_my_anime_my_is_rewatching(self):
-        self.assertIsInstance(self.anime.__my_is_rewatching, bool)
+        self.assertIsInstance(self.anime.my_is_rewatching, bool)
 
-    def my_completed_episodes(self):
-        self.assertIsInstance(self.anime.__my_completed_episodes, int)
+    def test_my_manga_my_completed_episodes(self):
+        self.assertIsInstance(self.anime.my_completed_episodes, int)
 
     def test_my_anime_my_score(self):
-        self.assertIsInstance(self.anime.__my_score, int)
+        self.assertIsInstance(self.anime.my_score, int)
 
     def test_my_anime_my_start_date(self):
-        self.assertTrue(self.anime.__my_start_date == float('inf') or type(self.anime.__my_start_date) == int)
+        self.assertTrue(self.anime.my_start_date == float('inf') or type(self.anime.my_start_date) == int)
 
     def test_my_anime_my_end_date(self):
-        self.assertTrue(self.anime.__my_end_date == float('inf') or type(self.anime.__my_end_date) == int)
+        self.assertTrue(self.anime.my_end_date == float('inf') or type(self.anime.my_end_date) == int)
 
     def test_my_anime_my_priority(self):
-        self.assertIsInstance(self.anime.__my_priority, int)
+        self.assertIsInstance(self.anime.my_priority, int)
 
     def test_my_anime_my_storage_type(self):
-        self.assertIsInstance(self.anime.__my_storage_type, int)
+        self.assertIsInstance(self.anime.my_storage_type, int)
 
     def test_my_anime_my_storage_value(self):
-        self.assertIsInstance(self.anime.__my_storage_value, int)
+        self.assertIsInstance(self.anime.my_storage_value, int)
 
     def test_my_anime_my_download_episodes(self):
-        self.assertIsInstance(self.anime.__my_download_episodes, int)
+        self.assertIsInstance(self.anime.my_download_episodes, int)
 
     def test_my_anime_my_times_rewatched(self):
-        self.assertIsInstance(self.anime.__my_times_rewatched, int)
+        self.assertIsInstance(self.anime.my_times_rewatched, int)
 
     def test_my_anime_my_rewatch_value(self):
-        self.assertIsInstance(self.anime.__my_rewatch_value, int)
+        self.assertIsInstance(self.anime.my_rewatch_value, int)
 
 
 def main():
--- tests/test_my_manga.py
+++ tests/test_my_manga.py
@@ -9,8 +9,47 @@ class MyMangaTestCase(unittest.TestCase):
         cls.account = Account(ACCOUNT_TEST_USERNAME, ACCOUNT_TEST_PASSWORD)
         cls.manga = cls.account.mangas[0]
 
-    def test_my_manga(self):
-        self.manga.my_score
+    def test_my_manga_my_id(self):
+        self.assertIsInstance(self.manga.my_id, int)
+
+    def test_my_manga_my_status(self):
+        self.assertIsInstance(self.manga.my_status, int)
+
+    def test_my_manga_my_is_rereading(self):
+        self.assertIsInstance(self.manga.my_is_rereading, bool)
+
+    def test_my_manga_my_completed_chapters(self):
+        self.assertIsInstance(self.manga.my_completed_chapters, int)
+
+    def test_my_manga_my_completed_volumes(self):
+        self.assertIsInstance(self.manga.my_completed_volumes, int)
+
+    def test_my_manga_my_score(self):
+        self.assertIsInstance(self.manga.my_score, int)
+
+    def test_my_manga_my_start_date(self):
+        self.assertTrue(self.manga.my_start_date == float('inf') or type(self.manga.my_start_date) == int)
+
+    def test_my_manga_my_end_date(self):
+        self.assertTrue(self.manga.my_end_date == float('inf') or type(self.manga.my_end_date) == int)
+
+    def test_my_manga_my_priority(self):
+        self.assertIsInstance(self.manga.my_priority, int)
+
+    def test_my_manga_my_storage_type(self):
+        self.assertIsInstance(self.manga.my_storage_type, int)
+
+    def test_my_manga_my_storage_value(self):
+        self.assertIsInstance(self.manga.my_storage_value, int)
+
+    def test_my_manga_my_download_episodes(self):
+        self.assertIsInstance(self.manga.my_download_episodes, int)
+
+    def test_my_manga_my_times_reread(self):
+        self.assertIsInstance(self.manga.my_times_reread, int)
+
+    def test_my_manga_my_reread_value(self):
+        self.assertIsInstance(self.manga.my_reread_value, int)
 
 def main():
     unittest.main()
