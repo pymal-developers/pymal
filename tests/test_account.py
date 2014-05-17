08cf76813f3b64a7d4cebb61c0448b583d98b93a
tomer gelber <tomergelber@gmail.com>
2014-05-17 20:45:53 +0300

    removed acount_ from tests name - its on the case!

--- tests/test_account.py
+++ tests/test_account.py
@@ -6,7 +6,7 @@ from tests.constants_for_testing import ACCOUNT_TEST_USERNAME, ACCOUNT_TEST_PASS
 
 
 class AccountInitTestCase(unittest.TestCase):
-    def test_account_init_not_auth(self):
+    def test_init_not_auth(self):
         account = Account(ACCOUNT_TEST_USERNAME)
         self.assertFalse(account.is_auth)
 
@@ -14,18 +14,18 @@ class AccountInitTestCase(unittest.TestCase):
         account = Account(ACCOUNT_TEST_USERNAME, ACCOUNT_TEST_PASSWORD)
         self.assertTrue(account.is_auth)
 
-    def test_account_init_auth_bad_password(self):
+    def test_init_auth_bad_password(self):
         account = Account(ACCOUNT_TEST_USERNAME, ACCOUNT_TEST_PASSWORD * 2)
         self.assertFalse(account.is_auth)
 
-    def test_account_init_later_auth(self):
+    def test_init_later_auth(self):
         account = Account(ACCOUNT_TEST_USERNAME)
         self.assertFalse(account.is_auth)
 
         account.change_password(ACCOUNT_TEST_PASSWORD)
         self.assertTrue(account.is_auth)
 
-    def test_account_init_later_auth_bad_password(self):
+    def test_init_later_auth_bad_password(self):
         account = Account(ACCOUNT_TEST_USERNAME)
         self.assertFalse(account.is_auth)
 
