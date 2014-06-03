bf540c64e6fb0f90c0e9e8f6b822faab3d8f7c7e
tomer gelber <tomergelber@gmail.com>
2014-06-03 23:28:43 +0300

    added friends list!

--- pymal/Account.py
+++ pymal/Account.py
@@ -31,6 +31,8 @@ class Account(object, metaclass=decorators.SingletonFactory):
     __ANIME_SEARCH_URL = __SEARCH_URL.format('anime')
     __MANGA_SEARCH_URL = __SEARCH_URL.format('manga')
 
+    __FRIENDS_URL = request.urljoin(consts.HOST_NAME, 'myfriends.php?o=2')
+
     __MY_LOGIN_URL = request.urljoin(consts.HOST_NAME, 'login.php')
     __DATA_FORM = 'username={0:s}&password={1:s}&cookie=1&sublogin=Login'
 
@@ -46,6 +48,7 @@ class Account(object, metaclass=decorators.SingletonFactory):
 
         self.__animes = None
         self.__mangas = None
+        self.__friends = None
 
         if password is not None:
             self.change_password(password)
@@ -61,6 +64,37 @@ class Account(object, metaclass=decorators.SingletonFactory):
         if self.__mangas is None:
             self.__mangas = AccountMangas.AccountMangas(self._username, self)
         return self.__mangas
+
+    @property
+    def friends(self) -> list:
+        if self.__friends is not None:
+            return self.__friends
+        friends = list()
+
+        div_wrapper = global_functions.get_content_wrapper_div(self.__FRIENDS_URL, self.auth_connect)
+        assert div_wrapper is not None
+
+        div_content = div_wrapper.find(name="div", recursive=False, attrs={"id": "content"})
+        assert div_content is not None
+
+        table_content = div_content.find(name="table")
+        assert table_content is not None
+
+        friends_rows = table_content.tbody.findAll(name="tr", recursive=False)
+        assert len(friends_rows) > 1
+        friends_rows = friends_rows[1:]
+
+        for friend_row in friends_rows:
+            div_pic = friend_row.find(name="div", attrs={'class': 'picSurround'})
+            assert div_pic is not None
+
+            splited_friend_url = div_pic.a['href'].split('/profile/', 1)
+            assert len(splited_friend_url) == 2
+
+            friends.append(Account(splited_friend_url[1]))
+
+        self.__friends = friends
+        return self.__friends
 
     def search(self, search_line: str, is_anime: bool=True) -> set:
         """
