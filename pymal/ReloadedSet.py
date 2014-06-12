7cc626c59cd1205c205900c39f61899a61ad3c04
tomer gelber <tomergelber@gmail.com>
2014-06-13 00:16:20 +0300

    added ReloadedSet

---
+++ pymal/ReloadedSet.py
@@ -0,0 +1,61 @@
+__authors__ = ""
+__copyright__ = "(c) 2014, pymal"
+__license__ = "BSD License"
+__contact__ = "Name Of Current Guardian of this file <email@address>"
+
+import collections
+
+from pymal import decorators
+
+
+class ReloadedSet(collections.Set):
+    @property
+    def _values(self):
+        raise NotImplemented()
+
+    def reload(self):
+        raise NotImplemented()
+
+    def __contains__(self, item):
+        return item in self._values
+
+    def __iter__(self):
+        return iter(self._values)
+
+    def __len__(self):
+        return len(self._values)
+
+    def issubset(self, other):
+        return self <= frozenset(other)
+
+    def issuperset(self, other):
+        return self >= frozenset(other)
+
+    def union(self, *others):
+        res = set(self)
+        for other in others:
+            res |= frozenset(other)
+        return res
+
+    def intersection(self, *others):
+        res = set(self)
+        for other in others:
+            res &= frozenset(other)
+        return res
+
+    def difference(self, *others):
+        res = set(self)
+        for other in others:
+            res -= frozenset(other)
+        return res
+
+    def symmetric_difference(self, other):
+        return set(self) ^ frozenset(other)
+
+
+class ReloadedSetSingletonFactoryType(type(ReloadedSet), decorators.SingletonFactory):
+    pass
+
+
+class ReloadedSetSingletonFactory(ReloadedSet, metaclass=ReloadedSetSingletonFactoryType):
+    pass
