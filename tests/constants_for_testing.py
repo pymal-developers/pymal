0bec8486180a2a9742f667e6de35fcd044322c4e
tomer gelber <tomergelber@gmail.com>
2014-06-20 18:41:06 +0300

    wrong IDs

--- tests/constants_for_testing.py
+++ tests/constants_for_testing.py
@@ -6,5 +6,5 @@ ACCOUNT_TEST_MAIL = "pymal-developer@mailinator.com"
 ACCOUNT_TEST_USERNAME = "pymal-developr"
 ACCOUNT_TEST_PASSWORD = "a0ataqett"
 
-ANIME_ID = 587
-MANGA_ID = 1887
+ANIME_ID = 1887
+MANGA_ID = 587
