9524d4cb283f0ab4d3bbe5d8032a2bdd96868975
tomer gelber <tomergelber@gmail.com>
2014-06-27 17:53:44 +0300

    moved import inside of functions if able and looks good.

--- pymal/searches/SearchAccounts.py
+++ pymal/searches/SearchAccounts.py
@@ -3,7 +3,6 @@ __copyright__ = "(c) 2014, pymal"
 __license__ = "BSD License"
 __contact__ = "Name Of Current Guardian of this file <email@address>"
 
-from pymal import Account
 from pymal.searches import Search
 
 __all__ = ['SearchAccounts']
@@ -13,5 +12,7 @@ class SearchAccounts(Search.Search):
     _SEARCH_NAME = 'users'
     _SEARCHED_URL_SUFFIX = '/profile/'
 
-    def _SEARCHED_OBJECT(self, mal_url: str) -> Account.Account:
+    def _SEARCHED_OBJECT(self, mal_url: str):
+        from pymal import Account
+
         return Account.Account(mal_url)
