a63a777a8817ae359ec46a5687d116186864d74f
tomer gelber <tomergelber@gmail.com>
2014-07-19 20:35:30 +0300

    added parse errors.

--- pymal/account_objects/AccountFriends.py
+++ pymal/account_objects/AccountFriends.py
@@ -7,6 +7,7 @@ __contact__ = "Name Of Current Guardian of this file <email@address>"
 from pymal import global_functions
 from pymal.types import ReloadedSet
 from pymal.decorators import load
+from pymal import exceptions
 
 
 class AccountFriends(ReloadedSet.ReloadedSetSingletonFactory):
@@ -29,8 +30,12 @@ class AccountFriends(ReloadedSet.ReloadedSetSingletonFactory):
         return self.__friends_list
 
     def reload(self):
+        """
+        :exception FailedToParseError
+        """
         div_wrapper = global_functions.get_content_wrapper_div(self.__url, self.account.connect)
-        assert div_wrapper is not None
+        if div_wrapper is None:
+            raise exceptions.FailedToParseError()
 
         list_div_friend = div_wrapper.findAll(name="div", attrs={"class": "friendBlock"})
         self.__friends = frozenset(map(self.__parse_friend_div, list_div_friend))
@@ -39,13 +44,25 @@ class AccountFriends(ReloadedSet.ReloadedSetSingletonFactory):
 
     @staticmethod
     def __parse_friend_div(div_friend):
+        """
+
+        :param div_friend:
+        :type div_friend:
+
+        :exception FailedToParseError
+
+        :return:
+        :rtype:
+        """
         from pymal import Account
 
         div_pic = div_friend.find(name="div", attrs={'class': 'picSurround'})
-        assert div_pic is not None
+        if div_pic is None:
+            raise exceptions.FailedToParseError(div_friend)
 
         splited_friend_url = div_pic.a['href'].split('/profile/', 1)
-        assert len(splited_friend_url) == 2
+        if 2 != len(splited_friend_url):
+            raise exceptions.FailedToParseError()
 
         return Account.Account(splited_friend_url[1])
 
