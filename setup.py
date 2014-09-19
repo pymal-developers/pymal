25d5702857e7d362b9b4565e4cac0786c7e0c845
tomer gelber <tomergelber@gmail.com>
2014-09-19 20:06:10 +0300

    using newer packages

--- setup.py
+++ setup.py
@@ -50,11 +50,11 @@ setup(
         'Topic :: Software Development :: User Interfaces',
     ],
     install_requires=[
-        'requests>=2.3.0',
+        'requests>=2.5.0',
         'beautifulsoup4>=4.3.2',
         'httpcache>=0.1.3',
         'html5lib>=0.999',
-        'six>=1.1,<1.4',
-        'pillow>=2.4.0',
+        'six==1.3',
+        'pillow>=2.5.3',
     ],
 )
