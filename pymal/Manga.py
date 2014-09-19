15b35b65757d8ac73111818be9febf7c1c34f0c3
tomer gelber <tomergelber@gmail.com>
2014-09-20 02:35:17 +0300

    fixed docstring of manga

--- pymal/Manga.py
+++ pymal/Manga.py
@@ -18,7 +18,7 @@ __all__ = ['Manga']
 
 class Manga(object, metaclass=SingletonFactory.SingletonFactory):
     """
-    Anime object that keeps all the anime data in MAL.
+    Object that keeps all the anime data in MAL.
 
     Properties:
      - id - int
