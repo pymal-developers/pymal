eccefc55631d09ff5881ffcdf310fef7a6c753fb
tomer gelber <tomergelber@gmail.com>
2014-09-20 23:00:47 +0300

    inner package naming fix

--- pymal/__init__.py
+++ pymal/__init__.py
@@ -3,7 +3,7 @@ __copyright__ = "(c) 2014, pymal"
 __license__ = "BSD License"
 __contact__ = "Name Of Current Guardian of this file <email@address>"
 
-__all__ = ['Account', 'Anime', 'Manga', 'Seasons']
+__all__ = ['account', 'anime', 'manga', 'seasons']
 
 
 def get_version():
