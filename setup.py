041ef37ba7a5abd4639536edbb06c11a163c7449
tomer gelber <tomergelber@gmail.com>
2014-10-03 15:44:42 +0300

    removed unneed line

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Database',
