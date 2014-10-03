18fcbde9eaaa490f8b797c9c413c444d09b8d4fc
tomer gelber <tomergelber@gmail.com>
2014-10-03 15:58:16 +0300

    splat requirements to regular and test. setup reading regular. added more dynamically calculating.

--- pymal/__init__.py
+++ pymal/__init__.py
@@ -1,7 +1,5 @@
-__authors__ = ""
-__copyright__ = "(c) 2014, pymal"
+__authors__ = "pymal-developers"
 __license__ = "BSD License"
-__contact__ = "Name Of Current Guardian of this file <email@address>"
 
 __all__ = ['account', 'anime', 'manga', 'seasons']
 
